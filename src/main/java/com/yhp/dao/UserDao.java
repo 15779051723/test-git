8e1d9bf39c75355ee046ed357c3f2b7699d009e6
yhp <2475198577@qq.com>
2020-11-02 12:05:27 +0800

    Initial commit

---
+++ src/main/java/com/yhp/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.yhp.dao;
+
+/**
+ * @author yhp
+ * @CreateDate 2020/11/2 - 11:49
+ * @Description:
+ */
+public class UserDao {
+
+    public void save() {
+        System.out.println("save......");
+    }
+
+}
